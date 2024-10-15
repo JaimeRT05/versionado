b4e76eae70f85eb2fc16072a3ab6ddf71d434564
JaimeRT05 <romantejerojaime@gmail.com>
2024-10-15 10:23:32 +0200

    Seguimos testeando el etiquetado de versiones

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ package GITIGNORE;
 public class HelloWorld {
     public static void main (String [] args) {
         System.out.println("Hola Mundo! from GITT");
+        System.out.println("Aquí estamos haciendo pruebas en Git");
     }
 }
